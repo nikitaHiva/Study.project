a80aa5c821f8175e379852e3e64d191b37162edb
Boss <nikitahivickiiy@gmal.com>
2019-12-24 15:40:46 +0300

    lesson10

---
+++ lesson10.php
@@ -0,0 +1,26 @@
+<form action="" method="GET">
+    <input type="text" name="name" placeholder="Введите имя"><br>
+    <input type="text" name="old" placeholder="Введите возраст"><br><br>
+    <textarea name="massage" cols="40" rows="10" placeholder="Сообщение"></textarea><br>
+	<input type="submit"> <br>
+</form>
+<?php
+//	if (!empty($_REQUEST['name'])) {
+//        $city = $_REQUEST['name'];
+//        echo 'Привет, '.$city;
+//    } else {
+//	    echo 'Введите имя';
+//    }
+//////////////////////////////////
+if (!empty($_REQUEST['name']) && !empty($_REQUEST['old']) && !empty($_REQUEST['massage']) ) {
+    $name = $_REQUEST['name'];
+    $old = $_REQUEST['old'];
+    $massage = $_REQUEST['massage'];
+    echo 'Привет, ', $name, ' ', $old, ' Лет', '<br>';
+    echo $massage;
+} else {
+    echo 'Введите данные заного';
+}
+
+
+
