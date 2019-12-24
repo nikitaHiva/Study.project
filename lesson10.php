24a77a54f5fb11c8aa306433bca379fbbf5dc206
Boss <nikitahivickiiy@gmal.com>
2019-12-24 16:37:15 +0300

    lesson1

--- lesson10.php
+++ lesson10.php
@@ -1,9 +1,3 @@
-<form action="" method="GET">
-    <input type="text" name="name" placeholder="Введите имя"><br>
-    <input type="text" name="old" placeholder="Введите возраст"><br><br>
-    <textarea name="massage" cols="40" rows="10" placeholder="Сообщение"></textarea><br>
-	<input type="submit"> <br>
-</form>
 <?php
 //	if (!empty($_REQUEST['name'])) {
 //        $city = $_REQUEST['name'];
@@ -12,15 +6,27 @@
 //	    echo 'Введите имя';
 //    }
 //////////////////////////////////
-if (!empty($_REQUEST['name']) && !empty($_REQUEST['old']) && !empty($_REQUEST['massage']) ) {
-    $name = strip_tags($_REQUEST['name']);
-    $old = strip_tags($_REQUEST['old']);
-    $massage = strip_tags($_REQUEST['massage']);
-    echo 'Привет, ', $name, ' ', $old, ' Лет', '<br>';
-    echo $massage;
+//if (!empty($_REQUEST['name']) && !empty($_REQUEST['old']) && !empty($_REQUEST['massage']) ) {
+//    $name = strip_tags($_REQUEST['name']);
+//    $old = strip_tags($_REQUEST['old']);
+//    $massage = strip_tags($_REQUEST['massage']);
+//    echo 'Привет, ', $name, ' ', $old, ' Лет', '<br>';
+//    echo $massage;
+//} else {
+//    echo 'Введите данные заного';
+//}
+////////////////////////////////////////////////////////////////////////////
+$login = 'nina225';
+$pas = 'qwerty';
+?>
+<form action="" method="get">
+    <input type="text" name="login" placeholder="Логин"><br>
+    <input type="password" name="pas" placeholder="Пароль"><br>
+    <input type="submit">
+</form>
+<?php
+if ($login == $_REQUEST['login'] && $pas == $_REQUEST['pas']) {
+    echo 'Пароль и логин подходят';
 } else {
-    echo 'Введите данные заного';
+    echo 'Либо пароль, либо логин неверный';
 }
-
-
-
